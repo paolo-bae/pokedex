000f92d1c15a6ece739392db03d8efd831333571
Paolo Baeli <paolo.baeli@gmail.com>
2022-06-11 20:36:09 +0200

    BottomSheet integration for PokemonDetailView -- in progress...

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -12,34 +12,48 @@ struct FlatLinkStyle: ButtonStyle {
         configuration.label
     }
 }
+
 struct ContentView: View {
+    
     @StateObject var vm = ViewModel()
+    @State var show = false
+    
     var body: some View {
-        NavigationView {
+        ZStack {
+            //MARK: Pokedex Home View
             ScrollView {
                 VStack(alignment: .leading) {
+                    Text("Pokédex")
+                        .font(.largeTitle)
+                        .foregroundColor(.red)
                     Text("The Pokédex contains detailed stats for every creature from the Pokémon games.")
                         .font(.subheadline)
                         .foregroundColor(.gray)
                     Spacer()
                     LazyVStack {
                         ForEach(vm.filteredPokemon, id: \.id) { pokemon in
-                            NavigationLink {
-                                PokemonDetailView()
-                            }
-                                label: {
-                                    PokemonCard(pokemon: pokemon)
-                                }.buttonStyle(FlatLinkStyle())
+                            
+                            PokemonCard(pokemon: pokemon)
+                                .onTapGesture {
+                                    withAnimation() {
+                                        show.toggle()
+                                    }
+                                }
                         }
                     }
                     .animation(.easeIn(duration: 0.3), value: vm.filteredPokemon.count)
                 }
-                .navigationBarTitle("Pokédex")
-                .searchable(text: $vm.searchText)
-                .padding(.horizontal)
             }
+            //MARK: Show Selected Pokemon Detail View
+//            if show {
+//                Color.black(opacity(0.3).ignoresSafeArea())
+//
+//                PokemonDetailView(pokemon: Pokemon.samplePokemon)
+//                    .transition(.move(edge: .bottom))
+//                    .zIndex(1)
+//            }
         }
-        
+        .padding(.horizontal)
     }
 }
 
--- Views/PokemonDetailView.swift
+++ Views/PokemonDetailView.swift
@@ -9,15 +9,38 @@ import SwiftUI
 
 struct PokemonDetailView: View {
     
+    @State var translation: CGSize = .zero
+    
     @EnvironmentObject var vm: ViewModel
     
+    let pokemon: Pokemon
+    
     var body: some View {
-        Text("POKEMON DETAIL")
+        VStack {
+            Text(pokemon.number)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(pokemon.type[0].getColor())
+        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .offset(y: translation.height)
+        .gesture(DragGesture()
+            .onChanged { value in
+                translation = value.translation
+            }
+            .onEnded { value in
+                withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.7)) {
+                    translation = .zero
+                }
+            }
+        )
+        .ignoresSafeArea(edges: .bottom)
+        
     }
 }
 
 struct PokemonDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        PokemonDetailView()
+        PokemonDetailView(pokemon: Pokemon.samplePokemon)
+            .background(.blue)
     }
 }
